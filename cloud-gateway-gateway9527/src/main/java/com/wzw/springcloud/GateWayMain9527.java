9692f46ad1581e83cf638adb2e7172d8a92b0694
AHUTwzw <954008805@qq.com>
2020-12-05 18:09:11 +0800

    添加gateway

---
+++ cloud-gateway-gateway9527/src/main/java/com/wzw/springcloud/GateWayMain9527.java
@@ -0,0 +1,17 @@
+package com.wzw.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author wuzhiwei
+ * @create 2020-12-05 16:19
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GateWayMain9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayMain9527.class);
+    }
+}
---
+++ cloud-gateway-gateway9527/src/main/java/com/wzw/springcloud/config/GatewayConfig.java
@@ -0,0 +1,29 @@
+package com.wzw.springcloud.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author wuzhiwei
+ * @create 2020-12-05 16:29
+ */
+@Configuration
+public class GatewayConfig {
+
+    /**
+     * 配置一个ID为 path_route_george 的路由规则
+     * 当访问地址:http://localhost:9527/guonei 时会自动转发到地址 http://news.baidu.com/guonei
+     *
+     * @param routeLocatorBuilder
+     * @return
+     */
+    @Bean
+    public RouteLocator customeRouteLocator(RouteLocatorBuilder routeLocatorBuilder) {
+        return routeLocatorBuilder.routes()
+                .route("path_route_george", r -> r.path("/wzw")
+                        .uri("http://news.baidu.com/wzw"))
+                .build();
+    }
+}
